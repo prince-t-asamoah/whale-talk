f62fb346d0e3b8560e265fc17c835b41f65c017f
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-01-02 07:50:10 +0000

    Initial commit

---
+++ whale-talk.js
@@ -0,0 +1,17 @@
+let input = 'luffy said he is gonna be the king of the pirates!';
+const vowels = ['a','e','i','o','u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  //console.log(i);
+  for (let j = 0; j < vowels.length; j++) {
+    //console.log(j);
+    if (input[i] === vowels[j]) {
+      resultArray.push(vowels[j]);
+    }
+  }
+    if (input[i] === 'e' || input[i] === 'u') {
+      resultArray.push(input[i]);
+    }
+}
+console.log(resultArray.join('').toUpperCase());
